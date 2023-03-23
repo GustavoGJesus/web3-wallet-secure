24dd7767fa521cba792f582e0eaa4690e20fd241
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-03-22 21:28:54 -0300

    feat: add main component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,14 @@
 // components 
 import { Header } from "../../components/header";
+import { Main } from "../../components/main";
 
 
 
 export function Home(){
     return(
+      <>
         <Header />
+        <Main />
+      </>
     );
 }
