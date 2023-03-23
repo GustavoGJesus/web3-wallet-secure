833536f196fec7b60cba5f68c7cf2b33e63bc20c
GustavoGJesus <ggomesdejesus01@gmail.com>
2023-03-23 01:51:54 -0300

    feat: shared datas

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,26 +1,50 @@
 // styles
-import { Button } from "../shared/button";
+// import { Button } from "../shared/button";
 import { ContainerAddress, Divider, HeaderContainer, HeaderContent } from "./styles";
 
 // libs
 import {AiOutlineUser } from 'react-icons/ai'
+import Modal from "../shared/modal";
+import useModal from "../../hooks/modal/useModal";
+import { Button, Input } from "../main/styles";
+
+// types 
+interface HeaderProps {
+  address: string;
+  private_key?: string;
+}
 
 
-export function Header() {
-	return (
+export function Header({ address, private_key }: HeaderProps) {
+  const { isOpen, toggle } = useModal();
+
+  return (
     <HeaderContainer>
       <HeaderContent>
         <h1>secure wallet</h1>
 
         <ContainerAddress>
-          <Button>Recovery Pharse</Button>
+          <Button
+            onClick={toggle}
+            style={{ background: "#cb3837", color: "#fff" }}
+          >
+            Recovery Pharse
+          </Button>
           <Divider />
           <p>
             {" "}
-            <AiOutlineUser /> 0x63677fbb34bh434834b48734hb34h4378478fb4
+            <AiOutlineUser /> {address}
           </p>
         </ContainerAddress>
       </HeaderContent>
+
+      <Modal isOpen={isOpen} toggle={toggle}>
+        <h4 style={{ fontSize: "25px" }}>Wallet Address</h4>
+        <div>{address}</div>
+
+        <h4>Wallet private key</h4>
+        <div style={{width: "200px"}}>{private_key}</div>
+      </Modal>
     </HeaderContainer>
   );
 }
